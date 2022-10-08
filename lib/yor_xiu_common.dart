1a3939dbdcb1b4d44849d65203ae1634bc67ebdf
tuhao <1008611.xxx>
2022-10-08 20:38:04 +0800

    开放方法

--- lib/yor_xiu_common.dart
+++ lib/yor_xiu_common.dart
@@ -18,11 +18,11 @@ class YorXiuCommon {
     return YorXiuCommonPlatform.instance.getPlatformVersion();
   }
 
-  Ref getRef(){
+  static Ref getRef(){
     return Config._ref;
   }
 
-  String getBaseUrl(){
+  static String getBaseUrl(){
     if(Config._ref == Ref.dev){
       return Config._devUrl;
     } else if(Config._ref == Ref.dev){
